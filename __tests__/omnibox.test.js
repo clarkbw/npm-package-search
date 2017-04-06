1422b201f8e072d462e96a9f28bb1ceb18323970
Bryan Clark <clarkbw@gmail.com>
2017-04-06 15:55:09 -0700

    mock the fetch calls

--- __tests__/omnibox.test.js
+++ __tests__/omnibox.test.js
@@ -4,50 +4,70 @@ import {
   handleInputEntered,
   SEARCH_API_URL,
   SEARCH_DEFAULT_URL
-} from '../src/omnibox';
+} from "../src/omnibox";
 
-describe('omnibox', () => {
-  it('should return a default suggestion', () => {
+describe("omnibox", () => {
+  it("should return a default suggestion", () => {
     expect(defaultSuggestion).toMatchObject({
       description: expect.any(String)
     });
   });
 
-  describe('handleInputChanged', () => {
-    xit('should return results from the query', () => {
-      const query = 'query';
-      const callback = jest.fn();
+  describe("handleInputChanged", () => {
+    it("should return results from the query", done => {
+      fetch.mockResponse(
+        JSON.stringify({
+          objects: [
+            {
+              package: {
+                name: "one",
+                links: {
+                  npm: "https://one"
+                }
+              }
+            }
+          ]
+        })
+      );
+      const query = "query";
+      const callback = function(results) {
+        expect(results).toHaveLength(1);
+        expect(results[0]).toMatchObject({
+          description: "one",
+          content: "https://one"
+        });
+        done();
+      };
       handleInputChanged(query, callback);
       expect(fetch).toHaveBeenCalled();
-      expect(callback).toBeCalledWith();
     });
   });
 
-  describe('handleInputEntered', () => {
-    it('should open in the current tab', () => {
-      const query = 'query';
-      expect(handleInputEntered(query, 'currentTab')).toBe(
+  describe("handleInputEntered", () => {
+    it("should open in the current tab", () => {
+      const query = "query";
+      expect(handleInputEntered(query, "currentTab")).toBe(
         `${SEARCH_DEFAULT_URL}${query}`
       );
       expect(chrome.tabs.update).toHaveBeenCalled();
     });
 
-    it('should open in the current tab', () => {
-      expect(handleInputEntered(SEARCH_API_URL, 'currentTab')).toBe(
+    it("should open in the current tab", () => {
+      expect(handleInputEntered(SEARCH_API_URL, "currentTab")).toBe(
         SEARCH_API_URL
       );
       expect(chrome.tabs.update).toHaveBeenCalled();
     });
 
-    it('should open in a new foreground tab', () => {
-      expect(handleInputEntered(SEARCH_API_URL, 'newForegroundTab')).toEqual({
+    it("should open in a new foreground tab", () => {
+      expect(handleInputEntered(SEARCH_API_URL, "newForegroundTab")).toEqual({
         url: SEARCH_API_URL
       });
       expect(chrome.tabs.create).toHaveBeenCalled();
     });
 
-    it('should open in a new foreground tab', () => {
-      expect(handleInputEntered(SEARCH_API_URL, 'newBackgroundTab')).toEqual({
+    it("should open in a new foreground tab", () => {
+      expect(handleInputEntered(SEARCH_API_URL, "newBackgroundTab")).toEqual({
         url: SEARCH_API_URL,
         active: false
       });
